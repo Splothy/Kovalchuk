9aaf2f5abb15f9423d5b438b5cb64432e21f8ad5
Splothy <yaroslav.o.kovalchuk@oa.edu.ua>
2026-03-16 11:44:57 +0200

    feat:

--- vue-lab/src/App.vue
+++ vue-lab/src/App.vue
@@ -1,20 +1,47 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <div class="max-w-4xl mx-auto">
-      <nav class="p-4 bg-gray-100 rounded-b-lg shadow-md">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/contact">Contact</RouterLink>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-      </nav>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
     </div>
   </header>
 
-  <main class="max-w-4xl mx-auto mt-5">
-    <RouterView />
+  <main>
+    <TheWelcome />
   </main>
 </template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
